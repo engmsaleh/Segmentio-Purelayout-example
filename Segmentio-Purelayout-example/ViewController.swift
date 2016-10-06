d4ed2274f07aa84555e3a2a2eece12cb5677e4cb
Mohamed Saleh <mohamed.saleh@ventaapps.com>
2016-10-06 17:22:11 +0200

    Initial Commit

---
+++ Segmentio-Purelayout-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segmentio-Purelayout-example
+//
+//  Created by Mohamed Saleh on 10/6/16.
+//  Copyright © 2016 Venta Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
